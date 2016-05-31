5488681a5f3c7090e8c68af1cfb726664479edf2
Colin W Kingen and Tony Fabery <tony@>
2016-05-31 16:42:30 -0700

    edited scripts.js to be more concise.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,81 @@
 $(function() {
-  $('.clickable1').click(function() {
-    $('.hidden#javascript').toggle("hidden");
-    $('.shown#javascript').toggle("shown");
+  $('.clickable').click(function() {
+    $(this).children().toggle();
   });
-  $('.clickable2').click(function() {
-    $('.hidden#operators').toggle("hidden");
-    $('.shown#operators').toggle("shown");
-  });
-  $('.clickable3').click(function() {
-    $('.hidden#variables').toggle("shown");
-    $('.shown#variables').toggle("hidden");
-  });
-  $('.clickable4').click(function() {
-    $('.hidden#conventions').toggle("shown");
-    $('.shown#conventions').toggle("hidden");
-  });
-  $('.clickable5').click(function() {
-    $('.hidden#functions').toggle("shown");
-    $('.shown#functions').toggle("hidden");
-  });
-  $('.clickable6').click(function() {
-    $('.hidden#methods').toggle("shown");
-    $('.shown#methods').toggle("hidden");
-  });
-  $('.clickable7').click(function() {
-    $('.hidden#arguments').toggle("shown");
-    $('.shown#arguments').toggle("hidden");
-  });
-  $('.clickable8').click(function() {
-    $('.hidden#parameters').toggle("shown");
-    $('.shown#parameters').toggle("hidden");
-  });
-  $('.clickable9').click(function() {
-    $('.hidden#return').toggle("shown");
-    $('.shown#return').toggle("hidden");
-  });
-  $('.clickable10').click(function() {
-    $('.hidden#chaining').toggle("shown");
-    $('.shown#chaining').toggle("hidden");
-  });
-  $('.clickable11').click(function() {
-    $('.hidden#strings').toggle("shown");
-    $('.shown#strings').toggle("hidden");
-  });
-  $('.clickable12').click(function() {
-    $('.hidden#booleans').toggle("shown");
-    $('.shown#booleans').toggle("hidden");
-  });
-  $('.clickable13').click(function() {
-    $('.hidden#undefined').toggle("shown");
-    $('.shown#undefined').toggle("hidden");
-  });
-  $('.clickable14').click(function() {
-    $('.hidden#nan').toggle("shown");
-    $('.shown#nan').toggle("hidden");
-  });
-  $('.clickable15').click(function() {
-    $('.hidden#escape').toggle("shown");
-    $('.shown#escape').toggle("hidden");
-  });
-  $('.clickable16').click(function() {
-    $('.hidden#alert').toggle("shown");
-    $('.shown#alert').toggle("hidden");
-  });
-  $('.clickable17').click(function() {
-    $('.hidden#comments').toggle("shown");
-    $('.shown#comments').toggle("hidden");
-  });
-  $('.clickable18').click(function() {
-    $('.hidden#jquery').toggle("shown");
-    $('.shown#jquery').toggle("hidden");
-  });
-  $('.clickable19').click(function() {
-    $('.hidden#attributes').toggle("shown");
-    $('.shown#attributes').toggle("hidden");
-  });
+  // $('.clickable1').click(function() {
+  //   $('.hidden#javascript').toggle("hidden");
+  //   $('.shown#javascript').toggle("shown");
+  // });
+  // $('.clickable2').click(function() {
+  //   $('.hidden#operators').toggle("hidden");
+  //   $('.shown#operators').toggle("shown");
+  // });
+  // $('.clickable3').click(function() {
+  //   $('.hidden#variables').toggle("shown");
+  //   $('.shown#variables').toggle("hidden");
+  // });
+  // $('.clickable4').click(function() {
+  //   $('.hidden#conventions').toggle("shown");
+  //   $('.shown#conventions').toggle("hidden");
+  // });
+  // $('.clickable5').click(function() {
+  //   $('.hidden#functions').toggle("shown");
+  //   $('.shown#functions').toggle("hidden");
+  // });
+  // $('.clickable6').click(function() {
+  //   $('.hidden#methods').toggle("shown");
+  //   $('.shown#methods').toggle("hidden");
+  // });
+  // $('.clickable7').click(function() {
+  //   $('.hidden#arguments').toggle("shown");
+  //   $('.shown#arguments').toggle("hidden");
+  // });
+  // $('.clickable8').click(function() {
+  //   $('.hidden#parameters').toggle("shown");
+  //   $('.shown#parameters').toggle("hidden");
+  // });
+  // $('.clickable9').click(function() {
+  //   $('.hidden#return').toggle("shown");
+  //   $('.shown#return').toggle("hidden");
+  // });
+  // $('.clickable10').click(function() {
+  //   $('.hidden#chaining').toggle("shown");
+  //   $('.shown#chaining').toggle("hidden");
+  // });
+  // $('.clickable11').click(function() {
+  //   $('.hidden#strings').toggle("shown");
+  //   $('.shown#strings').toggle("hidden");
+  // });
+  // $('.clickable12').click(function() {
+  //   $('.hidden#booleans').toggle("shown");
+  //   $('.shown#booleans').toggle("hidden");
+  // });
+  // $('.clickable13').click(function() {
+  //   $('.hidden#undefined').toggle("shown");
+  //   $('.shown#undefined').toggle("hidden");
+  // });
+  // $('.clickable14').click(function() {
+  //   $('.hidden#nan').toggle("shown");
+  //   $('.shown#nan').toggle("hidden");
+  // });
+  // $('.clickable15').click(function() {
+  //   $('.hidden#escape').toggle("shown");
+  //   $('.shown#escape').toggle("hidden");
+  // });
+  // $('.clickable16').click(function() {
+  //   $('.hidden#alert').toggle("shown");
+  //   $('.shown#alert').toggle("hidden");
+  // });
+  // $('.clickable17').click(function() {
+  //   $('.hidden#comments').toggle("shown");
+  //   $('.shown#comments').toggle("hidden");
+  // });
+  // $('.clickable18').click(function() {
+  //   $('.hidden#jquery').toggle("shown");
+  //   $('.shown#jquery').toggle("hidden");
+  // });
+  // $('.clickable19').click(function() {
+  //   $('.hidden#attributes').toggle("shown");
+  //   $('.shown#attributes').toggle("hidden");
+  // });
 });
